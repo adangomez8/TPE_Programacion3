73c2db13f41caa09c0fc280f58a89cace05061ff
adangomez8 <adanvgomez1234@gmail.com>
2023-05-06 08:58:07 -0300

    tpe

---
+++ tpe/GrafoDirigido.java
@@ -0,0 +1,163 @@
+package Programacion3.src.tpe;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class GrafoDirigido<T> implements Grafo<T> {
+
+	private HashMap<Integer, ArrayList<Arco<T>>> grafo;
+
+	public GrafoDirigido(){
+		this.grafo = new HashMap<>();
+	}
+
+	@Override
+	public void agregarVertice(int verticeId) {
+		if(!grafo.containsKey(verticeId)) {
+			grafo.put(verticeId, new ArrayList<Arco<T>>());
+		}
+	}
+
+	@Override
+	public void borrarVertice(int verticeId) {
+
+			grafo.remove(verticeId);
+	}
+
+	@Override
+	public void agregarArco(int verticeId1, int verticeId2, T etiqueta) {
+		crearArco(verticeId1, verticeId2, etiqueta);
+	}
+
+	private void crearArco(int verticeId1, int verticeId2, T etiqueta) {
+		if(grafo.containsKey(verticeId1)  && grafo.containsKey(verticeId2)) {
+			Arco<T> newArco = new Arco<>(verticeId1, verticeId2, etiqueta);
+			grafo.get(verticeId1).add(newArco);
+		}
+
+	}
+
+	@Override
+	public void borrarArco(int verticeId1, int verticeId2) {
+
+		if (grafo.containsKey(verticeId1)) {
+
+			ArrayList<Arco<T>> listAux = grafo.get(verticeId1);
+			boolean arcoBorrado = false;
+
+			while (!arcoBorrado){
+
+			if (listAux != null && !listAux.isEmpty()) {
+				for (int i = 0; i < listAux.size(); i++) {
+					int destinoAux = listAux.get(i).getVerticeDestino();
+					if (destinoAux == verticeId2) {
+						listAux.remove(i);
+						arcoBorrado = true;
+					}
+				}
+			}
+			}
+		}
+	}
+
+	@Override
+	public boolean contieneVertice(int verticeId) {
+
+		return grafo.containsKey(verticeId);
+	}
+
+	@Override
+	public boolean existeArco(int verticeId1, int verticeId2) {
+
+		for (Map.Entry<Integer, ArrayList<Arco<T>>> entry : grafo.entrySet()) {
+			if(entry.getKey()==verticeId1){
+				ArrayList<Arco<T>> listAux = grafo.get(entry.getKey());
+				for(Arco<T> arco : listAux){
+					if(arco.getVerticeDestino() == verticeId2){
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public Arco<T> obtenerArco(int verticeId1, int verticeId2) {
+
+		ArrayList<Arco<T>> resultado = grafo.get(verticeId1);
+
+		if(resultado != null && !resultado.isEmpty()) {
+			for (Arco<T> arco : resultado) {
+				if (arco.getVerticeDestino() == verticeId2) {
+
+					return arco;
+				}
+			}
+
+		}
+		return null;
+	}
+
+	@Override
+	public int cantidadVertices() {
+
+		return grafo.size();
+	}
+
+	@Override
+	public int cantidadArcos() {
+
+		int totalArcos = 0;
+		for (Map.Entry<Integer, ArrayList<Arco<T>>> entry : grafo.entrySet()) {
+			ArrayList<Arco<T>> listAux = entry.getValue();
+			totalArcos += listAux.size();
+		}
+		return totalArcos;
+	}
+
+	@Override
+	public Iterator<Integer> obtenerVertices() {
+
+		return grafo.keySet().iterator();
+	}
+
+	@Override
+	public Iterator<Integer> obtenerAdyacentes(int verticeId) {
+
+		ArrayList<Arco<T>> arcos = grafo.get(verticeId);
+		ArrayList<Integer> adyacentes = new ArrayList<Integer>();
+		if (arcos != null) {
+			for (Arco<T> arco : arcos) {
+				adyacentes.add(arco.getVerticeDestino());
+			}
+		}
+		return adyacentes.iterator();
+	}
+
+	@Override
+	public Iterator<Arco<T>> obtenerArcos() {
+
+		ArrayList<Arco<T>> arcos = new ArrayList<>();
+		for(Map.Entry<Integer, ArrayList<Arco<T>>> entry : grafo.entrySet()){
+			arcos.addAll(entry.getValue());
+		}
+		return arcos.iterator();
+	}
+
+	@Override
+	public Iterator<Arco<T>> obtenerArcos(int verticeId) {
+
+		ArrayList<Arco<T>> arcos = new ArrayList<>();
+		for(Map.Entry<Integer, ArrayList<Arco<T>>> entry : grafo.entrySet()){
+			if(entry.getKey()==verticeId) {
+				arcos.addAll(entry.getValue());
+			}
+		}
+		return arcos.iterator();
+	}
+
+}
---
+++ tpe/Main.java
@@ -0,0 +1,118 @@
+package Programacion3.src.tpe;
+
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GrafoDirigido<String> gd = new GrafoDirigido<>();
+
+        gd.agregarVertice(16);
+        gd.agregarVertice(34);
+        gd.agregarVertice(52);
+        gd.agregarVertice(7);
+
+        gd.agregarArco(16, 7, "pe");
+        gd.agregarArco(34, 7, "se");
+        gd.agregarArco(34, 16, "te");
+        gd.agregarArco(52, 16, "ce");
+
+        System.out.println("Contiene vértice 34: " + gd.contieneVertice( 34));
+        System.out.println("Contiene vértice 61: " + gd.contieneVertice(61));
+        System.out.println("Existe arco (16, 7) :" + gd.existeArco(16, 7));
+        System.out.println("Existe arco (7, 16) :" + gd.existeArco(7, 16));
+        System.out.println("Arco de origen 52 y destino 16: " + gd.obtenerArco(52,16));
+        System.out.println("Arco de origen 52 y destino 7: " + gd.obtenerArco(16,52));
+        System.out.println("Cantidad de vértices: " + gd.cantidadVertices());
+        System.out.println("Cantidad de arcos: " + gd.cantidadArcos());
+
+        System.out.print("Vértices del grafo: ");
+        Iterator<Integer> iteradorTodosLosVertices = gd.obtenerVertices();
+        while (iteradorTodosLosVertices.hasNext()) {
+            System.out.print(iteradorTodosLosVertices.next() + " ");
+        }
+        System.out.println();
+
+        System.out.print("Adyacentes del vértice 34: ");
+        Iterator<Integer> iteradorAdyacentes34 = gd.obtenerAdyacentes(34);
+        while (iteradorAdyacentes34.hasNext()) {
+            System.out.print(iteradorAdyacentes34.next() + " ");
+        }
+        System.out.println();
+
+        System.out.println("Adyacentes del vértice 7: ");
+        Iterator<Integer> iteradorAdyacentes7 = gd.obtenerAdyacentes(7);
+        while (iteradorAdyacentes7.hasNext()) {
+            System.out.print(iteradorAdyacentes7.next() + " ");
+        }
+
+        System.out.println("Cantidad de arcos del grafo: " + gd.cantidadArcos());
+
+        System.out.print("Arcos del vértice 34: ");
+        Iterator<Arco<String>> iteradorArcosDelVertice34 = gd.obtenerArcos(34);
+        while (iteradorArcosDelVertice34.hasNext()) {
+            System.out.print(iteradorArcosDelVertice34.next() + " ");
+        }
+        System.out.println();
+
+        System.out.println("Arcos del vértice 7: ");
+        Iterator<Arco<String>> iteradorArcosDelVertice7 = gd.obtenerArcos(7);
+        while (iteradorArcosDelVertice7.hasNext()) {
+            System.out.print(iteradorArcosDelVertice7.next() + " ");
+        }
+        System.out.println();
+
+        /**
+         *
+         * SE ELIMINA UN VERTICE
+         *
+         * */
+
+        gd.borrarVertice(34);
+        System.out.println("Contiene vértice 16: " + gd.contieneVertice( 16));
+        System.out.println("Contiene vértice 34: " + gd.contieneVertice(34));
+        System.out.println("Existe arco (16, 7) :" + gd.existeArco(16, 7));
+        System.out.println("Existe arco (7, 16) :" + gd.existeArco(7, 16));
+        System.out.println("Arco de origen 52 y destino 16: " + gd.obtenerArco(52,16));
+        System.out.println("Arco de origen 52 y destino 7: " + gd.obtenerArco(16,52));
+        System.out.println("Cantidad de vértices: " + gd.cantidadVertices());
+        System.out.println("Cantidad de arcos: " + gd.cantidadArcos());
+
+        System.out.print("Vértices del grafo: ");
+        Iterator<Integer> iteradorTodosLosVertices_a = gd.obtenerVertices();
+        while (iteradorTodosLosVertices_a.hasNext()) {
+            System.out.print(iteradorTodosLosVertices_a.next() + " ");
+        }
+        System.out.println();
+
+        System.out.print("Adyacentes del vértice 34: ");
+        Iterator<Integer> iteradorAdyacentes34_a = gd.obtenerAdyacentes(34);
+        while (iteradorAdyacentes34_a.hasNext()) {
+            System.out.print(iteradorAdyacentes34_a.next() + " ");
+        }
+        System.out.println();
+
+        System.out.println("Adyacentes del vértice 7: ");
+        Iterator<Integer> iteradorAdyacentes7_a = gd.obtenerAdyacentes(7);
+        while (iteradorAdyacentes7_a.hasNext()) {
+            System.out.print(iteradorAdyacentes7_a.next() + " ");
+        }
+
+        System.out.println("Cantidad de arcos del grafo: " + gd.cantidadArcos());
+
+        System.out.print("Arcos del vértice 34: ");
+        Iterator<Arco<String>> iteradorArcosDelVertice34_a = gd.obtenerArcos(34);
+        while (iteradorArcosDelVertice34_a.hasNext()) {
+            System.out.print(iteradorArcosDelVertice34_a.next() + " ");
+        }
+        System.out.println();
+
+        System.out.println("Arcos del vértice 7: ");
+        Iterator<Arco<String>> iteradorArcosDelVertice7_a = gd.obtenerArcos(7);
+        while (iteradorArcosDelVertice7_a.hasNext()) {
+            System.out.print(iteradorArcosDelVertice7_a.next() + " ");
+        }
+        System.out.println();
+    }
+}
